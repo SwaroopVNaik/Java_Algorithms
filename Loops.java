497aa606e8ab5f74f232c5b02cbefb4140a5b68b
Swaroop V Naik <swaroop.v.naik@gmail.com>
2026-09-03 18:22:28 +0530

    Loops

---
+++ Loops.java
@@ -0,0 +1,138 @@
+public class Loops {
+    public static void main(String[] args) {
+        
+        int index;
+
+        // Stepping Forward By One
+
+        // for(index = 1; index <= 5; index++)
+        // {   
+        //     // Block
+        //     System.out.println("Algorithms365");
+        // }
+
+        // Stepping Forward by Two
+
+        // for(index = 1; index <= 10; index += 2)
+        // {
+        //     // For Block
+        //     System.out.println(index);
+        // }
+
+        // Stepping Forward by Three
+
+        // for(index = 1; index <= 10; index += 3)
+        // {
+        //     // For Block
+        //     System.out.println(index);
+        // }
+
+        // Stepping Forward by Four 
+
+        // for(index = 1; index <= 10; index +=4)
+        // {
+        //     // For block
+        //     System.out.println(index);
+        // }
+
+        // Stepping Backward by One 
+
+        // for(index = 10; index >= 1; index -=1)
+        // {
+        //     System.out.println(index);
+        // }
+
+        // Steping Backward By Two
+
+        // for(index = 10; index >=1; index -=2)
+        // {
+        //     System.out.println(index);
+        // }
+
+        // Stepping Backward by Three
+
+        // for(index = 10; index >= 1; index -=3)
+        // {
+        //     System.out.println(index);
+        // }
+
+        // Stepping Backward By Four
+
+        // for(index = 10; index >= 1; index -=4)
+        // {
+        //     System.out.println(index);
+        // }
+
+        // Forward Alphabet Printing
+
+        char charcater;
+
+        // for(charcater = 'A'; charcater <= 'Z'; charcater++)
+        // {
+        //     System.out.println(charcater);
+        // }
+
+        // Reverse/Backward Alphabet Printing
+
+        // for(charcater = 'Z'; charcater >= 'A'; charcater--)
+        // {
+        //     System.out.println(charcater);
+        // }
+
+        // Print 1 to 100 
+
+        // for(index = 1; index <= 100; index++)
+        // {
+        //     System.out.println(index);
+        // }
+
+        // Printing 5 x 1;
+
+        // int Multiplication;
+
+        // for(index = 1; index <= 10; index++)
+        // {
+
+        //     Multiplication = 5 * index;
+        //     System.out.println("5 X "+ index + ": " + Multiplication);
+
+        // }
+
+        // Printing Square Numbers
+
+        // int Square;
+
+        // for(index = 1; index <= 10; index++)
+        // {
+        //     Square = index * index;
+        //     System.out.println("Square Number : " + Square);
+        // }
+
+        // Print Cube Numbers 
+
+        // int Cubes;
+
+        // for(index = 1; index <= 10; index++)
+        // {
+        //     Cubes = index * index * index;
+        //     System.out.println("Square Number : " + Cubes);
+        // }
+
+        // Sum Of First - N Natural Numbers 
+
+        int Sum;
+        int result;
+
+        for (index = 1; index <= 10; index++)
+        {
+
+            
+
+        }
+
+
+
+
+
+    }
+}
