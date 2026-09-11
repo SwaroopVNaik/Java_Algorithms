056381c21020e9bfa56bc4396687c2aba82cadbc
Swaroop V Naik <swaroop.v.naik@gmail.com>
2026-09-11 13:06:49 +0530

    MethodCalls

---
+++ MethodCall.java
@@ -0,0 +1,18 @@
+public class MethodCall
+{
+
+    static void welcome(){
+        greet();
+    }
+
+    static void greet(){
+        System.out.println("Welcome");
+    }
+
+    public static void main(String[] args) {
+        
+        welcome();
+
+    }
+
+}
