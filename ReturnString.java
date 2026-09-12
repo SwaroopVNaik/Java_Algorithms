ba28143e9269d46ac4ba06a692e3b25149c2ea9e
Swaroop V Naik <swaroop.v.naik@gmail.com>
2026-09-12 13:47:48 +0530

    PassByValue (String)

---
+++ ReturnString.java
@@ -0,0 +1,17 @@
+public class ReturnString 
+{
+
+    static void Welcome(String name1)
+    {
+        System.out.println("Welcome  " + name1);
+    }
+
+    public static void main(String[] args) 
+    {
+        
+        String name = "Swaroop";
+        Welcome(name);
+
+    }
+
+}
