48ed79b00d020b648e26b701cce0b7e77819563b
Swaroop V Naik <swaroop.v.naik@gmail.com>
2026-09-14 02:03:26 +0530

    QuestionTen

---
+++ Assignments/QuestionTen.java
@@ -0,0 +1,32 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class QuestionTen 
+{
+
+    // static void SearchFirstMatch()
+    // {
+
+    //     for(int index = 0; index <= Arr; index = index + 1)
+    //     {
+    //         if(index == 2){
+    //             System.out.println("Found at Index 2");
+    //             break;
+    //     }
+    //     }
+    // }
+
+    public static void main(String[] args)
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number to get from array");
+        // int number[] = sc.next();
+
+        // Pending Bro
+
+    }
+
+}
