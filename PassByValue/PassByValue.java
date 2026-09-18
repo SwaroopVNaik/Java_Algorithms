39ab5e6b734f9cdbbbdaf8ee1e74d9ff1a47a211
Swaroop V Naik <swaroop.v.naik@gmail.com>
2026-09-19 01:15:37 +0530

    Add PassByValue class to demonstrate pass by value concept

---
+++ PassByValue/PassByValue.java
@@ -0,0 +1,19 @@
+package PassByValue;
+
+public class PassByValue {
+
+    static int number(int number)
+    {
+        return number * 2;
+    }
+    
+    public static void main(String[] args) {
+
+        int number = 10;
+
+        int result = number(number);
+        System.out.println(result);
+
+    }
+
+}
