e464ff0fac427977a8ff543dacfe80236df82941
Swaroop V Naik <swaroop.v.naik@gmail.com>
2026-09-22 15:22:32 +0530

    LinearSearch For each

---
+++ For_Each_And_Arrays/LinearSearch.java
@@ -0,0 +1,28 @@
+package For_Each_And_Arrays;
+
+public class LinearSearch 
+{
+
+    static Boolean iskeyfound(int [] arr, int key)
+    {
+
+        for(int x : arr)
+        {
+            if(x == key)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) 
+    {
+
+        int[] arr = {10, 20, 30, 40, 50};
+        System.out.println(iskeyfound(arr, 30));
+        System.out.println(iskeyfound(arr, 100));
+
+    }
+
+}
