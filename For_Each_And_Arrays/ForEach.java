326afc7203f2e0e51c3e8d931e0d410b6c484313
Swaroop V Naik <swaroop.v.naik@gmail.com>
2026-09-22 15:22:10 +0530

    For each

---
+++ For_Each_And_Arrays/ForEach.java
@@ -0,0 +1,27 @@
+package For_Each_And_Arrays;
+
+public class ForEach 
+{
+
+    static void traverse(int[] arr)
+    {
+        for(int x : arr)
+        {
+            System.out.print(x + ",");
+            // implement if for to remove last coma (length - 1)
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+
+        // Creating array without refrence varible is called as anonyms array 
+        // new int[] {1, 2, 3, 4}
+
+        int [] arr = {1, 2, 3, 4};
+        traverse(arr);
+
+        // traverse(new int[] {1, 2, 3, 4});
+    }
+
+}
