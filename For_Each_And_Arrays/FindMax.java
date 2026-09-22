1652321a0d198354218fba2dfe5a8778dfc3717e
Swaroop V Naik <swaroop.v.naik@gmail.com>
2026-09-22 15:21:59 +0530

    For each find Max

---
+++ For_Each_And_Arrays/FindMax.java
@@ -0,0 +1,26 @@
+package For_Each_And_Arrays;
+
+public class FindMax {
+
+    static int findMax(int[] arr)
+    {
+        int max = arr[0];
+
+        for(int x : arr)
+        {
+            if(x > max)
+            {
+                max = x;
+            }
+        }
+        return max;
+    }
+    
+    public static void main(String[] args) {
+        
+        int[] arr = {10, 40, 90, 3};
+        System.out.println(findMax(arr));
+
+    }
+
+}
