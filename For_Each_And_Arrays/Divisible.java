4a2c18422968ebcd8de268feb3059c8b027318b2
Swaroop V Naik <swaroop.v.naik@gmail.com>
2026-09-22 15:21:49 +0530

    Divsible For each

---
+++ For_Each_And_Arrays/Divisible.java
@@ -0,0 +1,26 @@
+package For_Each_And_Arrays;
+
+public class Divisible 
+{
+
+    void printDivisibleNumber(int[] arr)
+    {
+        for(int number : arr)
+        {
+            if(number%3 == 0)
+            {
+                System.out.println(number);
+            }
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30};
+
+        Divisible obj = new Divisible();
+
+        obj.printDivisibleNumber(arr);
+    }
+
+}
