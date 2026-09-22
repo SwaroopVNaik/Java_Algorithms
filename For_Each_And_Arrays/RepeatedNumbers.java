16cac786da4f36b04e59d9f6b31178305bf57209
Swaroop V Naik <swaroop.v.naik@gmail.com>
2026-09-22 15:24:06 +0530

    RepeatedNumbers foreach

---
+++ For_Each_And_Arrays/RepeatedNumbers.java
@@ -0,0 +1,42 @@
+package For_Each_And_Arrays;
+
+import java.util.Scanner;
+
+public class RepeatedNumbers 
+{
+
+    void countNumberOfOccurence(int[] arr,int key)
+    {
+
+        int Count = 0;
+
+        for(int number : arr)
+        {
+            if(key == number)
+            {
+                Count = Count + 1;
+            }
+        }
+
+        System.out.println("Times Repeated :" + Count);
+    }
+
+    public static void main(String[] args) 
+    {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = {1, 3, 4, 7, 9, 9, 4, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 1, 1, 1, 1};
+
+        RepeatedNumbers obj = new RepeatedNumbers();
+
+        System.out.println("Enter the Number to check how many times the number repeats :");
+        int key = scanner.nextInt();
+
+        obj.countNumberOfOccurence(arr, key);
+        
+
+    scanner.close();
+    }
+
+}
